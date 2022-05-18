100583e10b00d80c79f722f327374d544cf07ac4
Felipe Roriz <f.roriz@hotmail.com>
2022-05-17 23:04:51 -0300

    Inclusão do exercício Cachorro

---
+++ ExerciciosCasa/ExercicioAula02/src/Animal/Animal.java
@@ -0,0 +1,42 @@
+package Animal;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		Cachorro c1, c2, c3;
+
+		c1 = new Cachorro();
+		c2 = new Cachorro();
+		c3 = new Cachorro();
+
+		c1.emitirSom();
+		
+		c3.dormir();
+		
+		//Não Funciona
+		//System.out.println(c1.emitirSom());
+
+		// Chamando o metodo nomecachorro()
+		c1.nomecachorro("Afosinho");
+
+		c1.comer();
+		
+		c1.tamanho = 100;
+		c1.emitirSom();
+		
+		//Mostrando a cor por encapsulamento
+		c2.setchamaCor("Esverdeada");
+		c1.setchamaCor("Preta");
+
+		
+		// Mostrando sem encapsulamento
+		//c2.cor = "Azul";
+		//System.out.println(c2.cor);
+		
+		System.out.println("A cor do cachorro c2 e " + c2.getmostraCor());
+		
+		System.out.println("A cor do cachorro c1 e "+ c1.iraRetornaraCor());
+
+	}
+
+}
