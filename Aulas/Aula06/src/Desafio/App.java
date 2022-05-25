2c45c5f001b95bcb2f3b5d296d74f3442c66c431
Felipe Roriz <f.roriz@hotmail.com>
2022-05-24 22:45:22 -0300

    Atualização do desafio da aula 06

--- Aulas/Aula06/src/Desafio/App.java
+++ Aulas/Aula06/src/Desafio/App.java
@@ -6,27 +6,30 @@ import java.util.Scanner;
 public class App {
 
 	public static void main(String[] args) {
-		String tec;
-		String temp;
-		
+		int tec;
+		int temp;
+
 		Scanner teclado = new Scanner(System.in);
+		Random gerador = new Random();
+
+		tec = 0;
+		temp = 1;
 		
-		temp = "Casa";
-		
-		System.out.println(temp);
-		
-		do {
-		System.out.println("Digite a palavra a cima");
-		tec = teclado.next();
-		
-		if (tec.equals("Casa") ) {
-			
-			System.out.println (temp);
+		//if (tec != temp) {
+			while (tec != temp) {
+				System.out.println("Digite o numero a seguir: ");
+
+				temp = gerador.nextInt(999999) + 1 ;
+				System.out.println(temp);
+
+				tec = teclado.nextInt();
+
+				//System.out.println("Digite a palavra a cima");
+			}System.out.println("FOI!");
+		//} else {
+			//System.out.println("AFONSO");
 		}
-		}while(tec.equals(""));
-		}
-	
+
 	}
 
-			
-		
+//}
