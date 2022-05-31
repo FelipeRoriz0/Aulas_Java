369975d49f319a02ffbc29ae975a3585f0cb21a4
Felipe Roriz <f.roriz@hotmail.com>
2022-05-30 22:24:21 -0300

    Atualização do exercício da aula 08

--- Aulas/Aula08/src/loja/App.java
+++ Aulas/Aula08/src/loja/App.java
@@ -4,34 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import amigo.Participante;
-
 public class App {
 
 	public static void main(String[] args) {
-		String nome, telefone, idade;
-		int numero1 = 1;
-		int numero2 = 2;
-		int numero3 = 3;
 		int respo;
+		String nome, telefone, idade;
+		Dependencias dp;
+		boolean teste = true;
 
-		List<String> Clientes = new ArrayList<String>();
-
-		System.out.println("1 - Ler Cliente");
-		System.out.println("2 - Listar Clientes");
-		System.out.println("3 - Fim");
-
+		List<Dependencias> clientes = new ArrayList<Dependencias>();
 		Scanner teclado = new Scanner(System.in);
-		respo = teclado.nextInt();
 
-		if (respo == 1) {
-			System.out.println("Nome Cliente");
-			nome = teclado.next();
+	//	do {
+			System.out.println("1 - Ler Cliente - 2 - Listar Clientes - 3 - Fim");
+			respo = teclado.nextInt();
 
-			Clientes.add(nome);
+			if (respo == 1) {
+				dp = new Dependencias();
 
-		}
+				System.out.println("Nome Cliente");
+				nome = teclado.next();
 
+				System.out.println("Qual o telefone");
+				telefone = teclado.next();
+
+				System.out.println("Qual a idade ?");
+				idade = teclado.next();
+
+				dp.setNome(nome);
+				dp.setTelefone(telefone);
+				dp.setIdade(idade);
+
+				clientes.add(dp);
+
+			} else if (respo == 2) {
+				for (Dependencias temp : clientes) {
+					System.out.println(temp.getNome());
+				}
+
+			}
+		//} while ();
 	}
 
 }
---
+++ Aulas/Aula08/src/loja/Dependencias.java
@@ -0,0 +1,26 @@
+package loja;
+
+public class Dependencias {
+	private String nome, telefone, idade;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getIdade() {
+		return idade;
+	}
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+	
+	int respo;
+
+}
