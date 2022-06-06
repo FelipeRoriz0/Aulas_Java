a41a9a256a7da73b3ba0b7e60702d730a43c4aa6
Felipe Roriz <F.roriz@hotmail.com>
2022-06-06 08:34:03 -0300

    Aula 11 - Java Web

---
+++ Aulas/Aula11/src/main/java/controle/LoginServlet.java
@@ -0,0 +1,55 @@
+package controle;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import servico.ServicoLogin;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+		String usuario = request.getParameter("usuario");
+		String senha = request.getParameter("senha");
+
+		ServicoLogin servico = new ServicoLogin();
+
+		if (servico.verificarLogin(usuario, senha)) {
+			response.sendRedirect("menu.html");
+		} else {
+			response.sendRedirect("erro.html");
+		}
+
+	}
+
+}
